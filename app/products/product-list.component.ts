4051d8a567ba606f483a7053bde92dedd60da6ad
Daniel Mangowi <daniel@.mangowicom>
2017-07-26 19:21:41 +0200

    build component product list

---
+++ app/products/product-list.component.ts
@@ -0,0 +1,10 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: 'app/products/produt-list.component.html'
+})
+export class ProductListComponent{
+
+}
