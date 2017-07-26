3f2fc9462aa3f893f8b7e83126a2eb417664637a
Daniel Mangowi <daniel@.mangowicom>
2017-07-26 18:39:56 +0200

    Create first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <h1>{{pageTitle}}</h1>
     `
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTile: string = "Acme Product Management";
+}
